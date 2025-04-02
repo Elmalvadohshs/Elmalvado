51b6b94d8c10767610eeb84a4aef7867d55eb455
Elmalvadohshs <kishophugad@gmail.com>
2025-04-02 09:25:35 +0800

    Update EmalvadoCpmTool.py

--- EmalvadoCpmTool.py
+++ EmalvadoCpmTool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from elmalvado import Tresehshs
+from elmalvado import Elmalvadohshs
 
 __CHANNEL_USERNAME__ = "EELMALVADOMAINCHANNEL"
 __GROUP_USERNAME__   = "ElmalvadoMainChat"
@@ -136,7 +136,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = Tresehshs(acc_access_key)
+        cpm = Elmalvadohshs(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
