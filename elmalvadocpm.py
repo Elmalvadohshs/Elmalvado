1e940009d3fc940bd31e2fadae25f84b23522ce3
Elmalvadohshs <kishophugad@gmail.com>
2025-05-20 02:47:26 +0800

    Update elmalvadocpm.py

--- elmalvadocpm.py
+++ elmalvadocpm.py
@@ -353,4 +353,15 @@ class Elmalvadohshs:
         response = requests.post(f"{__ENDPOINT_URL__}/speed_all_cars", params=params, data=payload)
         response_decoded = response.json()
         return response_decoded.get("ok")
+        
+    def spoiler(self, car_id, custom):
+        payload = {
+        "account_auth": self.auth_token,
+        "car_id": car_id,
+        "custom": custom,
+        }
+        params = {"key": self.access_key}
+        response = requests.post(f"{__ENDPOINT_URL__}/spoiler", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
  
